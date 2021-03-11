2447f6dddfc64ea46c4955a428977c5ad46fcbce
Aizhan Mambetalieva <aijan312kg@gmail.com>
2021-03-11 17:10:10 +0200

    Task1

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -99,6 +99,45 @@ public class SampleSteps {
         //throw new PendingException();
     }
 
+    @Given("^I am on number page$")
+    public void i_am_on_number_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        this.driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
+        //throw new PendingException();
+    }
+
+    @When("^I enter number: \"([^\"]*)\"$")
+    public void i_enter_number(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        WebElement ageText = this.driver.findElement(By.id("numb"));
+        ageText.sendKeys(new CharSequence[]{arg1});
+        //throw new PendingException();
+    }
+
+    @When("^I click submit number$")
+    public void i_click_submit_number() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        WebElement submitButton = this.driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/button"));
+        submitButton.click();
+        //throw new PendingException();
+    }
+
+    @Then("^I see a message: \"([^\"]*)\"$")
+    public void i_see_a_message(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        WebElement messageText = this.driver.findElement(By.id("ch1_error"));
+        Assert.assertEquals(arg1, messageText.getText());
+        //throw new PendingException();
+    }
+
+    @Then("^I see a new message: \"([^\"]*)\"$")
+    public void i_see_a_new_message(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        Alert alert = driver.switchTo().alert();
+        String message = alert.getText();
+        Assert.assertEquals(arg1, message);
+        //throw new PendingException();
+    }
 }
 
 
