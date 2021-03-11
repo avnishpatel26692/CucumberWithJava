521a6e6ed11e428ac84ae68737058c8f1ce6a18e
Tatjana Panova <tatutachi@gmail.com>
2021-03-11 14:48:36 +0200

    Sample 2 committed

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en_scouse.An;
+import org.junit.Assert;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -47,6 +48,69 @@ public class SampleSteps {
     public void iShouldSeeMenu() throws Throwable {
         assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
     }
+
+    @Given("^user open facebook page$")
+    public void user_open_facebook_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^user enters username and password$")
+    public void user_enters_username_and_password() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^user clicks on login button$")
+    public void user_clicks_on_login_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^user should be logged in successfully$")
+    public void user_should_be_logged_in_successfully() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User navigates to the page$")
+    public void user_navigates_to_the_page() throws Throwable {
+
+    }
+
+    @Then("^User should be verify page is opened correctly$")
+    public void user_should_be_verify_page_is_opened_correctly() throws Throwable {
+
+    }
+
+    @Given("^User navigates to Age Page$")
+    public void user_navigates_to_Age_Page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/age.html");
+    }
+
+    @When("^User enter name : \"([^\"]*)\"$")
+    public void user_enter_name(String arg1) throws Throwable {
+        WebElement nameText = driver.findElement(By.id("name"));
+        nameText.clear();
+        nameText.sendKeys(arg1);
+    }
+
+    @When("^User enter age : (\\d+)$")
+    public void user_enter_age(String arg1) throws Throwable {
+        WebElement ageText = driver.findElement(By.name("age"));
+        ageText.sendKeys(""+arg1);
+    }
+
+    @When("^Click on Submit button$")
+    public void click_on_Submit_button() throws Throwable {
+        driver.findElement(By.id("submit")).click();
+    }
+
+    @Then("^User see message : \"([^\"]*)\"$")
+    public void user_see_message(String arg1) throws Throwable {
+        Assert.assertEquals(arg1,driver.findElement(By.id("message")).getText());
+    }
+
 }
 
 
---
+++ src/test/resources/features/Sample2.feature
@@ -0,0 +1,13 @@
+Feature: Introduction to cucumber
+  As a test engineer I want to write and execute simple scenarios.
+
+  Scenario: Navigate to Link Page
+    When User navigates to the page
+    Then User should be verify page is opened correctly
+
+  Scenario: Check Age page functionality
+    Given User navigates to Age Page
+    When User enter name : "Nancy"
+    And User enter age : 29
+    And Click on Submit button
+    Then User see message : "Hello, Nancy, you are an adult"
