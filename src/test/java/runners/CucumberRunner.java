c1bdf7059f1ec414a5613836085abbe125822b9d
Natalja Sniega <natalja.protasova@gmail.com>
2021-03-11 17:05:26 +0200

    Last task (Task1.feature)

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -7,11 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/Sample3.feature", //provide feature file name(s), all other is pre-set
+        features = "src/test/resources/features/Task1.feature", //provide feature file name(s), all other is pre-set
         plugin = {"pretty", "html:cucumber-report/html-report",
                 "junit:cucumber-report/junit-report.xml",
                 "json:cucumber-report/json-report.json"},
-//        tags = {"@Regression"},
+//        tags = {"@NotWorking"},
         dryRun = false, //checks if there are step definitions. dryRun = true <<will scan all the Gherkins, won't run anything
         // if some don't have - it will create - u can copy prepared methods
         glue = {"stepDefinitions"}//location (path) for step definitions (in stepDefinitions folder)
--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -122,6 +122,32 @@ public class SampleSteps {
        Assert.assertEquals(arg1, driver.findElement(By.id("message")).getText());
     }
 
+    @Given("^I am on number page$")
+    public void i_am_on_number_page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
+    }
+
+    @When("^I enter number: \"([^\"]*)\"$")
+    public void i_enter_number(String arg1) throws Throwable {
+        WebElement ageText = driver.findElement(By.id("numb"));
+        ageText.sendKeys("" +arg1);
+    }
+
+    @When("^I click submit number$")
+    public void i_click_submit_number() throws Throwable {
+        driver.findElement(By.className("w3-btn")).click();
+    }
+
+    @Then("^I see a message: \"([^\"]*)\"$")
+    public void i_see_a_message(String arg1) throws Throwable {
+        Assert.assertEquals(arg1, driver.findElement(By.id("ch1_error")).getText());
+    }
+
+    @Then("^I see a new message: \"([^\"]*)\"$")
+    public void i_see_a_new_message(String arg2) throws Throwable {
+        Assert.assertEquals(arg2, driver.switchTo().alert().getText());
+    }
+
 
 }
 
