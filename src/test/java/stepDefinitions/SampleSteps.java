1ea10b46f21097f44a0f8df10b3f2308562ed0c7
Marija Rodionova <marija.limare@gmail.com>
2021-03-11 14:50:31 +0200

    Cucumber Activity

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en_scouse.An;
+import org.junit.Assert;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -47,6 +48,106 @@ public class SampleSteps {
     public void iShouldSeeMenu() throws Throwable {
         assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
     }
+
+    @Given("^user open facebook page$")
+    public void user_open_facebook_page() throws Throwable {
+
+    }
+
+    @When("^user enters username and password$")
+    public void user_enters_username_and_password() throws Throwable {
+
+    }
+
+    @When("^user clicks on login button$")
+    public void user_clicks_on_login_button() throws Throwable {
+
+    }
+
+    @Then("^user should be logged successfully$")
+    public void user_should_be_logged_successfully() throws Throwable {
+
+    }
+
+    @Then("^user can logout$")
+    public void user_can_logout() throws Throwable {
+
+    }
+
+    @And("^user is on home page$")
+        public void userOnHomePage() throws Throwable {
+        throw new PendingException();
+    }
+
+    @When("^User navigates to link page$")
+    public void userNavigatesToLinkPage() {
+        driver.get("https://kristinek.github.io/site/examples/po");
+    }
+
+    @Then("^User should verify page is opened correctly$")
+    public void userShouldVerifyPageIsOpenedCorrectly() {
+        Assert.assertEquals("Lorem ipsum",driver.findElement(By.xpath("//h1[text()='Lorem ipsum']")).getText());
+    }
+
+    @Given("^User navigates to Age page$")
+    public void userNavigatesToAgePage() {
+        driver.get("https://kristinek.github.io/site/examples/age");
+    }
+
+    @When("^user enter name : \"([^\"]*)\"$")
+    public void userEnterName(String arg1) throws Throwable {
+       WebElement nameText = driver.findElement(By.id("name"));
+       nameText.clear();
+       nameText.sendKeys(arg1);
+    }
+
+    @And("^user enter age : \"([^\"]*)\"$")
+    public void userEnterAge(int arg1) throws Throwable {
+        WebElement ageText = driver.findElement(By.id("age"));
+        ageText.sendKeys("" +arg1);
+
+    }
+
+    @And("^click Submit button$")
+    public void clickSubmitButton() {
+        driver.findElement(By.id("submit")).click();
+    }
+
+    @Then("^user see message : \"([^\"]*)\"$")
+    public void userSeeMessage(String arg1) throws Throwable {
+        Assert.assertEquals(arg1, driver.findElement(By.id("message")).getText());
+
+    }
+
+
+    @Given("^User navigate to AgePage$")
+    public void userNavigateToAgePage() {
+        driver.get("https://kristinek.github.io/site/examples/age");
+    }
+
+    @When("^User enters name : \"([^\"]*)\"$")
+    public void userEntersName(String arg1) throws Throwable {
+        WebElement nameText = driver.findElement(By.id("name"));
+        nameText.clear();
+        nameText.sendKeys(arg1);
+    }
+
+    @And("^User enters age : \"([^\"]*)\"$")
+    public void userEntersAge(int arg1) throws Throwable {
+        WebElement ageText = driver.findElement(By.id("age"));
+        ageText.sendKeys("" + arg1);
+    }
+
+    @And("^Click on submit button$")
+    public void clickOnSubmitButton() {
+        driver.findElement(By.id("submit")).click();
+    }
+
+    @Then("^User see the message : \"([^\"]*)\"$")
+    public void userSeeTheMessage(String arg1) throws Throwable {
+        Assert.assertEquals(arg1, driver.findElement(By.id("message")).getText());
+    }
+
 }
 
 
--- src/test/resources/features/Sample1.feature
+++ src/test/resources/features/Sample1.feature
@@ -8,3 +8,12 @@ Feature: Introduction to cucumber part 1
     Then I should see home page header
     And I should see home page description
     And I should see menu
+
+    Scenario: simple scenario1
+      Given user open facebook page
+      When user enters username and password
+      And user clicks on login button
+      Then user should be logged successfully
+      And user can logout
+      And user is on home page
+      And user open facebook page
