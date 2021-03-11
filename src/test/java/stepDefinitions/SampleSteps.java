d1c6858f3c406d7fdd1e06bcd6171883892f26a7
Viktorija Kolotova <viktorija.kolotova@gmail.com>
2021-03-11 14:47:59 +0200

    Exercise 1, Age Page

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en_scouse.An;
+import org.junit.Assert;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -47,6 +48,83 @@ public class SampleSteps {
     public void iShouldSeeMenu() throws Throwable {
         assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
     }
+
+    @Given("^user open facebook page$")
+    public void user_open_facebook_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^user enters username and password$")
+    public void user_enters_username_and_password() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^user clicks on login button$")
+    public void user_clicks_on_login_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^user should be logged in successfully$")
+    public void user_should_be_logged_in_successfully() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+    @Then("^user can logout$")
+    public void user_can_logout() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^user is on home page$")
+    public void user_is_on_home_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^user navigates to Link Page$")
+    public void user_navigates_to_Link_Page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/po");
+    }
+
+    @Then("^user should verify page is opened correctly$")
+    public void user_should_verify_page_is_opened_correctly() throws Throwable {
+        Assert.assertEquals("Lorem ipsum", driver.findElement(By.xpath("//h1[text()='Lorem ipsum']")).getText());
+
+    }
+    @Given("^user navigates to Age Page$")
+    public void user_navigates_to_Age_Page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/age.html");
+    }
+
+    @Then("^user enter name : \"([^\"]*)\"$")
+    public void user_enter_name(String arg1) throws Throwable {
+        WebElement nameText = driver.findElement(By.name("name"));
+        nameText.clear();
+        nameText.sendKeys(arg1);
+    }
+
+    @Then("^user enters age : (\\d+)$")
+    public void user_enters_age(int arg1) throws Throwable {
+       WebElement nameText = driver.findElement(By.name("age"));
+       nameText.sendKeys(""+arg1);
+    }
+
+    @Then("^click on Submit button$")
+    public void click_on_Submit_button() throws Throwable {
+        driver.findElement(By.id("submit")).click();
+    }
+
+    @Then("^user see message : \"([^\"]*)\"$")
+    public void user_see_message(String arg1) throws Throwable {
+        Assert.assertEquals(arg1, driver.findElement(By.id("message")).getText());
+    }
+
+
+
+
 }
 
 
--- src/test/resources/features/Sample1.feature
+++ src/test/resources/features/Sample1.feature
@@ -2,9 +2,18 @@ Feature: Introduction to cucumber part 1
   As a test engineer
   I want to be able to write and execute a simple scenario
 
-  @Sample1
+
   Scenario: Simple scenario
     When I am on the home page
     Then I should see home page header
     And I should see home page description
     And I should see menu
+
+
+    Scenario: Simple scenario 1
+      Given user open facebook page
+      When user enters username and password
+      And user clicks on login button
+      Then user should be logged in successfully
+      And user can logout
+      And user is on home page
