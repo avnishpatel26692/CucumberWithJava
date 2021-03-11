be48712ad41f69cc89c1608cbf655e1a5008a6f0
Tatjana Panova <tatutachi@gmail.com>
2021-03-11 17:14:56 +0200

    Task 1 committed

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -47,7 +47,7 @@ public class SampleSteps {
     @And("^I should see menu$")
     public void iShouldSeeMenu() throws Throwable {
         assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
-      }
+    }
 
     @When("^User navigates to the page$")
     public void user_navigates_to_the_page() throws Throwable {
@@ -56,7 +56,7 @@ public class SampleSteps {
 
     @Then("^User should verify that page is opened correctly$")
     public void user_should_be_verify_page_is_opened_correctly() throws Throwable {
-        Assert.assertEquals("Lorem ipsum",driver.findElement(By.xpath("//h1[text()='Lorem ipsum']")).getText());
+        Assert.assertEquals("Lorem ipsum", driver.findElement(By.xpath("//h1[text()='Lorem ipsum']")).getText());
     }
 
     @Given("^User navigates to Age Page$")
@@ -74,7 +74,7 @@ public class SampleSteps {
     @When("^User enter age : (\\d+)$")
     public void user_enter_age(String arg1) throws Throwable {
         WebElement ageText = driver.findElement(By.name("age"));
-        ageText.sendKeys(""+arg1);
+        ageText.sendKeys("" + arg1);
     }
 
     @When("^Click on Submit button$")
@@ -84,10 +84,39 @@ public class SampleSteps {
 
     @Then("^User see message : \"([^\"]*)\"$")
     public void user_see_message(String arg1) throws Throwable {
-        Assert.assertEquals(arg1,driver.findElement(By.id("message")).getText());
+        Assert.assertEquals(arg1, driver.findElement(By.id("message")).getText());
     }
 
+    @Given("^I am on number page$")
+    public void i_am_on_number_page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/tasks/enter_a_number");
+
+    }
+
+    @When("^I enter number: \"([^\"]*)\"$")
+    public void i_enter_number(String arg1) throws Throwable {
+        WebElement enterNumber = driver.findElement(By.id("numb"));
+        enterNumber.sendKeys("" + arg1);
+
+    }
+
+    @When("^I click submit number$")
+    public void i_click_submit_number() throws Throwable {
+        WebElement submitBtn = driver.findElement(By.xpath("//button[text()='Submit']"));
+        submitBtn.click();
+
+    }
+
+    @Then("^I see a message: \"([^\"]*)\"$")
+    public void i_see_a_message(String arg1) throws Throwable {
+        Assert.assertEquals(arg1, driver.findElement(By.id("ch1_error")).getText());
+
+    }
+
+    @Then("^I see a new message: \"([^\"]*)\"$")
+    public void i_see_a_new_message(String arg1) throws Throwable {
+        Assert.assertEquals(arg1, driver.switchTo().alert().getText());
+    }
+
+
 }
-
-
-
