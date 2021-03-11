bdf8cfb35a5d2ffd0ee52d5535e3ee4f19ef490e
Arnis Kolocans <arnisk999@gmail.com>
2021-03-11 14:43:18 +0200

    Initial commit. Sample 2 file added

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en_scouse.An;
+import org.junit.Assert;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -46,6 +47,47 @@ public class SampleSteps {
     @And("^I should see menu$")
     public void iShouldSeeMenu() throws Throwable {
         assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
+    }
+
+    @Given("^User navigates to Age Page$")
+    public void user_navigates_to_Age_Page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/age");
+    }
+
+    @When("^User enter name : \"([^\"]*)\"$")
+    public void user_enter_name(String arg1) throws Throwable {
+       WebElement name = driver.findElement(By.id("name"));
+       name.clear();
+       name.sendKeys(arg1);
+    }
+
+    @When("^User enter age : (\\d+)$")
+    public void user_enter_age(int arg1) throws Throwable {
+        WebElement age = driver.findElement(By.id("age"));
+
+        age.sendKeys(""+arg1);
+
+    }
+
+    @When("^Click on Submit button$")
+    public void click_on_Submit_button() throws Throwable {
+        driver.findElement(By.id("Submit")).click();
+    }
+
+    @Then("^User see message : \"([^\"]*)\"$")
+    public void user_see_message(String arg1) throws Throwable {
+        WebElement response = driver.findElement(By.id("message"));
+        Assert.assertEquals(arg1,response.getText());
+    }
+    @When("^User navigates to Link Page$")
+    public void user_navigates_to_Link_Page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/po");
+    }
+
+    @Then("^user should verify page is opened correctly$")
+    public void user_should_verify_page_is_opened_correctly() throws Throwable {
+        WebElement head1text = driver.findElement(By.xpath("//h1[text()='Lorem ipsum']"));
+        Assert.assertEquals("Lorem ipsum", head1text.getText());
     }
 }
 
---
+++ src/test/resources/features/Sample2.feature
@@ -0,0 +1,20 @@
+Feature: Check age page
+  Check age page functionality.
+
+  Scenario: Check kid Age page functionality
+    Given User navigates to Age Page
+    When User enter name : "Bill"
+    And User enter age : 2
+    And Click on Submit button
+    Then User see message : "Hello, Bill, you are a kid"
+
+  Scenario: Check adult Age page functionality
+    Given User navigates to Age Page
+    When User enter name : "Nancy"
+    And User enter age : 29
+    And Click on Submit button
+    Then User see message : "Hello, Nancy, you are an adult"
+
+  Scenario: Navigate to Link Page
+    When User navigates to Link Page
+    Then user should verify page is opened correctly
