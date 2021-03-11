50174e14dcd5ba72fb23f213796d4563cd4237c5
Viktorija Kolotova <viktorija.kolotova@gmail.com>
2021-03-11 16:15:55 +0200

    Exercise 2, ScenarioOutline Working, NotWorking

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         plugin = {"pretty", "html:cucumber-report/html-report",
                 "junit:cucumber-report/junit-report.xml",
                 "json:cucumber-report/json-report.json"},
-        //tags = {"@AgeScenario2"},
+        tags = {"@NotWorking"},
         dryRun = false,
         glue = {"stepDefinitions"}
 )
