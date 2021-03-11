fdd4de983db3f6d40f6ec1ff4a48fc4548c672e4
Anna Kombarova <charmomilla@mail.ru>
2021-03-11 17:29:57 +0200

    total push

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -1,6 +1,5 @@
 package stepDefinitions;
 
-
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -62,7 +61,7 @@ public class SampleSteps {
         nameText.sendKeys(arg1);
     }
 
-    @When("^User enter age : (\\d+)$")
+    @When("^User enter age : \"([^\"]*)\"$")
     public void user_enter_age(int arg1) throws Throwable {
         WebElement ageText = driver.findElement(By.id("age"));
         ageText.clear();
@@ -88,6 +87,37 @@ public class SampleSteps {
     public void user_should_verify_page_is_opened_correctly() throws Throwable {
         Assert.assertEquals("Lorem ipsum",driver.findElement(By.xpath("//h1")).getText());
     }
+    @Given("^I am on number page$")
+    public void i_am_on_number_page() throws Throwable {
+        driver.navigate().to("https://kristinek.github.io/site/tasks/enter_a_number");
+    }
+
+    @When("^I enter number: \"([^\"]*)\"$")
+    public void i_enter_number(String arg1) throws Throwable {
+        WebElement entNum = driver.findElement(By.id("numb"));
+        entNum.sendKeys(""+arg1);
+    }
+
+    @When("^I click submit number$")
+    public void i_click_submit_number() throws Throwable {
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/button")).click();
+
+    }
+
+    @Then("^I see a message: \"([^\"]*)\"$")
+    public void i_see_a_message(String arg1) throws Throwable {
+        WebElement msg = driver.findElement(By.id("ch1_error"));
+        Assert.assertEquals(arg1,msg.getText());
+    }
+
+    @Then("^I see a new message: \"([^\"]*)\"$")
+    public void i_see_a_new_message(String arg1) throws Throwable {
+        Alert msg = driver.switchTo().alert();
+        Assert.assertEquals(arg1,msg.getText());
+    }
+
+
 
 }
+
 
--- src/test/resources/features/Sample2.feature
+++ src/test/resources/features/Sample2.feature
@@ -2,7 +2,6 @@ Feature: Introduction to cucumber
   Some sample text here (definitions or stuff like that)
 
 
-
   Scenario: Navigate to Link Page
     When User navigates to Link Page
     Then user should verify page is opened correctly
@@ -13,7 +12,6 @@ Feature: Introduction to cucumber
     And User enter age : 21
     And Click on Submit button
     Then User see message : "Hello, John, you are an adult"
-
 
   Scenario: Check Age page functionality
     Given User navigates to Age Page
@@ -30,4 +28,3 @@ Feature: Introduction to cucumber
     And User enter age : 5
     And Click on Submit button
     Then User see message : "Hello, Ann, you are a kid"
-
