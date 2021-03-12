768b26d6044d043691e62fc1230b9924af2e372a
IR <irigerte@inbox.lv>
2021-03-12 14:14:31 +0200

    Steps4PO

---
+++ src/test/java/stepDefinitions/Steps4PO.java
@@ -0,0 +1,51 @@
+package stepDefinitions;
+
+import Pages.AgePage;
+import Pages.AgeSubmittedPage;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class Steps4PO {
+    private WebDriver driver;
+    AgePage agePO;
+    AgeSubmittedPage ageSubmittedPO;
+
+    public Steps4PO() {
+        this.driver = Hooks.driver;
+        agePO = PageFactory.initElements(this.driver, AgePage.class);
+        ageSubmittedPO = PageFactory.initElements(this.driver, AgeSubmittedPage.class);
+    }
+
+    @Given("^I am on age page using PO$")
+    public void I_am_on_age_page_using_PO() throws Throwable {
+        driver.get(agePO.agePageURL);
+    }
+
+    @When ("^I enter name using PO$")
+    public void I_enter_name_using_PO(String arg1) throws Throwable {
+     agePO.enterName(arg1);
+    }
+
+    @And ("^I enter age using PO$")
+    public void I_enter_age_using_PO(int arg1) throws Throwable {
+  agePO.enterAge("" +arg1);
+    }
+
+@And ("^I click submit using PO$")
+    public void I_click_submit_using_PO() throws Throwable {
+    agePO.clickOnSubmitBtn();
+}
+
+@Then ("^I see message using PO$")
+    public void I_see_message_using_PO(String arg1) throws Throwable {
+    Assert.assertEquals(arg1,ageSubmittedPO.getMessage());
+    }
+}
+
