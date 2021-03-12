ed597abdc42ace6a6ed5730ad962f47445815640
Egils Lapins <egilslapins@gmail.com>
2021-03-12 10:01:41 +0200

    Sample 4

--- cucumber-report/html-report/report.js
+++ cucumber-report/html-report/report.js
@@ -1,324 +1,261 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/Task1.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/Sample4.feature");
 formatter.feature({
   "line": 1,
-  "name": "Testing error cases and correct number case",
-  "description": "//#https://kristinek.github.io/site/tasks/enter_a_number",
-  "id": "testing-error-cases-and-correct-number-case",
+  "name": "Data Table Examples",
+  "description": "",
+  "id": "data-table-examples",
   "keyword": "Feature"
 });
-formatter.scenarioOutline({
-  "line": 5,
-  "name": "Testing error cases",
+formatter.before({
+  "duration": 1330951799,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "a new scenario with 1-row table",
   "description": "",
-  "id": "testing-error-cases-and-correct-number-case;testing-error-cases",
-  "type": "scenario_outline",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "line": 4,
-      "name": "@Scenario1"
-    }
-  ]
+  "id": "data-table-examples;a-new-scenario-with-1-row-table",
+  "type": "scenario",
+  "keyword": "Scenario"
 });
 formatter.step({
-  "line": 6,
-  "name": "I am on number page",
+  "line": 4,
+  "name": "I am on action page",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 7,
-  "name": "I enter number: \u003cnumber\u003e",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 8,
-  "name": "I click submit number",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 9,
-  "name": "I see a message: \u003cerrorMessage\u003e",
-  "keyword": "Then "
-});
-formatter.examples({
-  "line": 10,
-  "name": "",
-  "description": "",
-  "id": "testing-error-cases-and-correct-number-case;testing-error-cases;",
+  "line": 5,
+  "name": "I clicked on checkboxes:",
   "rows": [
     {
       "cells": [
-        "number",
-        "errorMessage"
+        "Option 1"
       ],
-      "line": 11,
-      "id": "testing-error-cases-and-correct-number-case;testing-error-cases;;1"
+      "line": 6
     },
     {
       "cells": [
-        "dad",
-        "Please enter a number"
+        "Option 3"
       ],
-      "line": 12,
-      "id": "testing-error-cases-and-correct-number-case;testing-error-cases;;2"
-    },
-    {
-      "cells": [
-        "101",
-        "Number is too big"
-      ],
-      "line": 13,
-      "id": "testing-error-cases-and-correct-number-case;testing-error-cases;;3"
-    },
-    {
-      "cells": [
-        "45",
-        "Number is too small"
-      ],
-      "line": 14,
-      "id": "testing-error-cases-and-correct-number-case;testing-error-cases;;4"
+      "line": 7
     }
   ],
-  "keyword": "Examples"
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "I click the result checkbox button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "message for checkboxes \"You selected value(s): Option 1, Option 3\" is seen",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Sample4Steps.i_am_on_action_page()"
+});
+formatter.result({
+  "duration": 866885600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Sample4Steps.i_clicked_on_checkboxes(String\u003e)"
+});
+formatter.result({
+  "duration": 83586500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Sample4Steps.i_click_the_result_checkbox_button()"
+});
+formatter.result({
+  "duration": 30952701,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "You selected value(s): Option 1, Option 3",
+      "offset": 24
+    }
+  ],
+  "location": "Sample4Steps.message_for_checkboxes_is_seen(String)"
+});
+formatter.result({
+  "duration": 21212699,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 3118358501,
+  "status": "passed"
 });
 formatter.before({
-  "duration": 1349099000,
+  "duration": 1017392701,
   "status": "passed"
 });
 formatter.scenario({
+  "line": 11,
+  "name": "a new scenario with 1-row table",
+  "description": "",
+  "id": "data-table-examples;a-new-scenario-with-1-row-table",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
   "line": 12,
-  "name": "Testing error cases",
-  "description": "",
-  "id": "testing-error-cases-and-correct-number-case;testing-error-cases;;2",
-  "type": "scenario",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "line": 4,
-      "name": "@Scenario1"
-    }
-  ]
-});
-formatter.step({
-  "line": 6,
-  "name": "I am on number page",
+  "name": "I am on action page",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 7,
-  "name": "I enter number: dad",
-  "matchedColumns": [
-    0
-  ],
-  "keyword": "When "
-});
-formatter.step({
-  "line": 8,
-  "name": "I click submit number",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 9,
-  "name": "I see a message: Please enter a number",
-  "matchedColumns": [
-    1
-  ],
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "SampleSteps.i_am_on_number_page()"
-});
-formatter.result({
-  "duration": 950379500,
-  "status": "passed"
-});
-formatter.match({
-  "location": "SampleSteps.i_enter_number_dad()"
-});
-formatter.result({
-  "duration": 57698400,
-  "status": "passed"
-});
-formatter.match({
-  "location": "SampleSteps.i_click_submit_number()"
-});
-formatter.result({
-  "duration": 31381800,
-  "status": "passed"
-});
-formatter.match({
-  "location": "SampleSteps.i_see_a_message_Please_enter_a_number()"
-});
-formatter.result({
-  "duration": 17449900,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 3090116100,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 1002470800,
-  "status": "passed"
-});
-formatter.scenario({
   "line": 13,
-  "name": "Testing error cases",
-  "description": "",
-  "id": "testing-error-cases-and-correct-number-case;testing-error-cases;;3",
-  "type": "scenario",
-  "keyword": "Scenario Outline",
-  "tags": [
+  "name": "I clicked on checkboxes:",
+  "rows": [
     {
-      "line": 4,
-      "name": "@Scenario1"
+      "cells": [
+        "Option 2"
+      ],
+      "line": 14
     }
-  ]
-});
-formatter.step({
-  "line": 6,
-  "name": "I am on number page",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 7,
-  "name": "I enter number: 101",
-  "matchedColumns": [
-    0
   ],
   "keyword": "When "
 });
 formatter.step({
-  "line": 8,
-  "name": "I click submit number",
+  "line": 15,
+  "name": "I click the result checkbox button",
   "keyword": "And "
 });
 formatter.step({
-  "line": 9,
-  "name": "I see a message: Number is too big",
-  "matchedColumns": [
-    1
-  ],
+  "line": 16,
+  "name": "message for checkboxes \"You selected value(s): Option 2\" is seen",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "SampleSteps.i_am_on_number_page()"
+  "location": "Sample4Steps.i_am_on_action_page()"
 });
 formatter.result({
-  "duration": 862903500,
+  "duration": 875542099,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Sample4Steps.i_clicked_on_checkboxes(String\u003e)"
+});
+formatter.result({
+  "duration": 45253200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Sample4Steps.i_click_the_result_checkbox_button()"
+});
+formatter.result({
+  "duration": 37697400,
   "status": "passed"
 });
 formatter.match({
   "arguments": [
     {
-      "val": "101",
-      "offset": 16
+      "val": "You selected value(s): Option 2",
+      "offset": 24
     }
   ],
-  "location": "SampleSteps.i_enter_number(int)"
+  "location": "Sample4Steps.message_for_checkboxes_is_seen(String)"
 });
 formatter.result({
-  "duration": 55233100,
-  "status": "passed"
-});
-formatter.match({
-  "location": "SampleSteps.i_click_submit_number()"
-});
-formatter.result({
-  "duration": 34327100,
-  "status": "passed"
-});
-formatter.match({
-  "location": "SampleSteps.i_see_a_message_Number_is_too_big()"
-});
-formatter.result({
-  "duration": 16758600,
+  "duration": 19676300,
   "status": "passed"
 });
 formatter.after({
-  "duration": 3089865800,
+  "duration": 3089347801,
   "status": "passed"
 });
 formatter.before({
-  "duration": 991609200,
+  "duration": 989110900,
   "status": "passed"
 });
 formatter.scenario({
-  "line": 14,
-  "name": "Testing error cases",
+  "line": 19,
+  "name": "a new scenario with 1-row table",
   "description": "",
-  "id": "testing-error-cases-and-correct-number-case;testing-error-cases;;4",
+  "id": "data-table-examples;a-new-scenario-with-1-row-table",
   "type": "scenario",
-  "keyword": "Scenario Outline",
-  "tags": [
-    {
-      "line": 4,
-      "name": "@Scenario1"
-    }
-  ]
+  "keyword": "Scenario"
 });
 formatter.step({
-  "line": 6,
-  "name": "I am on number page",
+  "line": 20,
+  "name": "I am on action page",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 7,
-  "name": "I enter number: 45",
-  "matchedColumns": [
-    0
+  "line": 21,
+  "name": "I clicked on checkboxes:",
+  "rows": [
+    {
+      "cells": [
+        "Option 1"
+      ],
+      "line": 22
+    },
+    {
+      "cells": [
+        "Option 2"
+      ],
+      "line": 23
+    },
+    {
+      "cells": [
+        "Option 3"
+      ],
+      "line": 24
+    }
   ],
   "keyword": "When "
 });
 formatter.step({
-  "line": 8,
-  "name": "I click submit number",
+  "line": 25,
+  "name": "I click the result checkbox button",
   "keyword": "And "
 });
 formatter.step({
-  "line": 9,
-  "name": "I see a message: Number is too small",
-  "matchedColumns": [
-    1
-  ],
+  "line": 26,
+  "name": "message for checkboxes \"You selected value(s): Option 1, Option 2, Option 3\" is seen",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "SampleSteps.i_am_on_number_page()"
+  "location": "Sample4Steps.i_am_on_action_page()"
 });
 formatter.result({
-  "duration": 869983500,
+  "duration": 879823600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Sample4Steps.i_clicked_on_checkboxes(String\u003e)"
+});
+formatter.result({
+  "duration": 113780599,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Sample4Steps.i_click_the_result_checkbox_button()"
+});
+formatter.result({
+  "duration": 27933000,
   "status": "passed"
 });
 formatter.match({
   "arguments": [
     {
-      "val": "45",
-      "offset": 16
+      "val": "You selected value(s): Option 1, Option 2, Option 3",
+      "offset": 24
     }
   ],
-  "location": "SampleSteps.i_enter_number(int)"
+  "location": "Sample4Steps.message_for_checkboxes_is_seen(String)"
 });
 formatter.result({
-  "duration": 50421100,
-  "status": "passed"
-});
-formatter.match({
-  "location": "SampleSteps.i_click_submit_number()"
-});
-formatter.result({
-  "duration": 35518200,
-  "status": "passed"
-});
-formatter.match({
-  "location": "SampleSteps.i_see_a_message_Number_is_too_small()"
-});
-formatter.result({
-  "duration": 15869300,
+  "duration": 18263199,
   "status": "passed"
 });
 formatter.after({
-  "duration": 3096888000,
+  "duration": 3092085800,
   "status": "passed"
 });
 });
