6c73a41340acdc151ebb642966d3b179ede6ce54
Arnis Kolocans <arnisk999@gmail.com>
2021-03-12 09:49:02 +0200

    Sample 4 updated, added sample 4 steps

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -122,28 +122,7 @@ public class SampleSteps {
         Assert.assertEquals(arg1,driver.switchTo().alert().getText());
     }
     //Sample 4
-    @Given("^I am on action page$")
-    public void i_am_on_action_page() throws Throwable {
-        driver.get("https://kristinek.github.io/site/examples/actions");
-    }
 
-    @When("^I clicked on checkboxes:$")
-    public void i_clicked_on_checkboxes(List<String> arg1) throws Throwable {
-        for(String value : arg1){
-            String xpath = "//input[@type='checkbox'][@value='" + value + "']";
-            driver.findElement(By.xpath(xpath)).click();
-        }
-    }
-
-    @When("^I click the result checkbox button$")
-    public void i_click_the_result_checkbox_button() throws Throwable {
-        driver.findElement(By.id("result_button_checkbox")).click();
-    }
-
-    @Then("^message for checkboxes \"([^\"]*)\" is seen$")
-    public void message_for_checkboxes_is_seen(String arg1) throws Throwable {
-        Assert.assertEquals(arg1,driver.findElement(By.id("result_checkbox")).getText());
-    }
 }
 
 
