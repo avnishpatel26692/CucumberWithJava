ce93fd2c75468be0ce94279e5e305da1d23ca575
avanish.patel <avanish.patel@accenture.com>
2021-03-12 09:50:13 +0200

    Sample 4 StepDef

---
+++ src/test/java/stepDefinitions/Sample4Steps.java
@@ -0,0 +1,50 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+
+public class Sample4Steps {
+
+    private WebDriver driver;
+
+    public Sample4Steps() {
+        this.driver = Hooks.driver;
+    }
+
+    @Given("^I am on action page$")
+    public void i_am_on_action_page() throws Throwable {
+        driver.get("https://kristinek.github.io/site/examples/actions");
+    }
+
+    @When("^I clicked on checkboxes:$")
+    public void i_clicked_on_checkboxes(List<String> arg1) throws Throwable {
+        for(String value : arg1)
+        {
+            String Start_xpath = "//input[@type='checkbox'][@value='";
+            String checkboxName = value;
+            String end_xpath = "']";
+            String xpath = Start_xpath + checkboxName + end_xpath;
+            System.out.println("Xpath : " + xpath);
+            driver.findElement(By.xpath(xpath)).click();
+        }
+
+    }
+
+    @When("^I click the result checkbox button$")
+    public void i_click_the_result_checkbox_button() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.id("result_button_checkbox")).click();
+    }
+
+    @Then("^message for checkboxes \"([^\"]*)\" is seen$")
+    public void message_for_checkboxes_is_seen(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        Assert.assertEquals(arg1, driver.findElement(By.id("result_checkbox")).getText());
+    }
+}
--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -118,36 +118,7 @@ public class SampleSteps {
         
     }
 
-    @Given("^I am on action page$")
-    public void i_am_on_action_page() throws Throwable {
-        driver.get("https://kristinek.github.io/site/examples/actions");
-    }
 
-    @When("^I clicked on checkboxes:$")
-    public void i_clicked_on_checkboxes(List<String> arg1) throws Throwable {
-        for(String value : arg1)
-        {
-            String Start_xpath = "//input[@type='checkbox'][@value='";
-            String checkboxName = value;
-            String end_xpath = "']";
-            String xpath = Start_xpath + checkboxName + end_xpath;
-            System.out.println("Xpath : " + xpath);
-            driver.findElement(By.xpath(xpath)).click();
-        }
-        
-    }
-
-    @When("^I click the result checkbox button$")
-    public void i_click_the_result_checkbox_button() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        driver.findElement(By.id("result_button_checkbox")).click();
-    }
-
-    @Then("^message for checkboxes \"([^\"]*)\" is seen$")
-    public void message_for_checkboxes_is_seen(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        Assert.assertEquals(arg1, driver.findElement(By.id("result_checkbox")).getText());
-    }
 
 
 
